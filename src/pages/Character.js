e5c98c3ba50d0ba0bc98cd0969595d8bd797b5a0
SantiagoDuque1700 <duquemoreno12@gmail.com>
2020-04-07 14:38:26 -0500

    estructurando

---
+++ src/pages/Character.js
@@ -0,0 +1,23 @@
+const Character = () => {
+    const view = `
+        <div class="Characters-inner">
+            <aricule class="Characters-card">
+                <img src="image" alt="name">
+                <h2>Name</h2>
+            </aricule>
+
+            <articule class="Characters-card">
+                <h3>Episodes:</h3>
+                <h3>Status:</h3>
+                <h3>Species:</h3>
+                <h3>Gender:</h3>
+                <h3>Origin:</h3>
+                <h3>Last Location:</h3>
+            </articule>
+        </div>
+    `;
+
+    return view;
+}
+
+export default Character;
